f11bf5877c39e1ccfdc49cbb1214fc9a7ddffe40
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-24 11:54:55 +0200

    ✨ Add instance list

--- src/instances.rs
+++ src/instances.rs
@@ -1,7 +1,26 @@
-use druid::{widget::Label, Widget};
+use std::fmt::format;
+
+use druid::{
+    widget::{Button, Flex, Label, List, Scroll},
+    Widget, WidgetExt,
+};
 
 use crate::AppState;
 
 pub fn build_widget() -> impl Widget<AppState> {
-    Label::new("Instances")
+    Flex::column()
+        .with_child(Label::new("Instances"))
+        .with_child(
+            Scroll::new(
+                List::new(|| {
+                    Flex::row()
+                        .with_child(Label::new(|name: &String, _env: &_| name.to_string()))
+                        .with_flex_spacer(1.)
+                        .with_child(Button::new("Launch"))
+                })
+                .with_spacing(10.)
+                .lens(AppState::instances),
+            )
+            .vertical(),
+        )
 }
