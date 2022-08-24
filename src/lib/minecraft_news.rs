fafd3e443c2ab3013347428ffff252db202f8b39
Manuel Quarneti <manuq01@pm.me>
2022-08-24 12:01:02 +0200

    🐛 Fix news

--- src/lib/minecraft_news.rs
+++ src/lib/minecraft_news.rs
@@ -6,15 +6,14 @@ use isahc::{Request, RequestExt, ReadResponseExt};
 const MINECRAFT_NEWS_URL: &str = "https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid";
 pub const MINECRAFT_NEWS_BASE_URL: &str = "https://www.minecraft.net";
 
-
 #[derive(Serialize, Deserialize)]
 pub struct News {
-    pub article_grid: Vec<Article>,
+    pub article_grid: Vec<ArticleGrid>,
     pub article_count: i64,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Article {
+pub struct ArticleGrid {
     pub default_tile: Tile,
     #[serde(rename = "articleLang")]
     pub article_lang: ArticleLang,
@@ -37,9 +36,20 @@ pub struct Tile {
 #[derive(Serialize, Deserialize)]
 pub struct Image {
     pub content_type: ContentType,
+    #[serde(rename = "videoURL")]
+    pub video_url: Option<String>,
+    #[serde(rename = "videoType")]
+    pub video_type: Option<String>,
     #[serde(rename = "imageURL")]
     pub image_url: String,
+    #[serde(rename = "videoProvider")]
+    pub video_provider: Option<String>,
     pub alt: Option<String>,
+    #[serde(rename = "videoId")]
+    pub video_id: Option<String>,
+    #[serde(rename = "linkurl")]
+    pub link_url: Option<String>,
+    pub background_color: Option<BackgroundColor>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -49,19 +59,37 @@ pub enum ArticleLang {
 }
 
 #[derive(Serialize, Deserialize)]
+pub enum BackgroundColor {
+    #[serde(rename = "bg-blue")]
+    BgBlue,
+    #[serde(rename = "bg-green")]
+    BgGreen,
+    #[serde(rename = "bg-red")]
+    BgRed,
+}
+
+#[derive(Serialize, Deserialize)]
 pub enum ContentType {
     #[serde(rename = "image")]
     Image,
+    #[serde(rename = "outgoing-link")]
+    OutgoingLink,
+    #[serde(rename = "video")]
+    Video,
 }
 
 #[derive(Serialize, Deserialize)]
 pub enum TileSize {
     #[serde(rename = "1x1")]
     The1X1,
+    #[serde(rename = "1x2")]
+    The1X2,
     #[serde(rename = "2x1")]
     The2X1,
     #[serde(rename = "2x2")]
     The2X2,
+    #[serde(rename = "4x2")]
+    The4X2,
 }
 
 /// Get the news from minecraft.net
