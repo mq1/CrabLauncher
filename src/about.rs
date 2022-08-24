9914174b77f0f2dad37b1146eab386468512284e
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-24 15:03:41 +0200

    ✨ New about page

--- src/about.rs
+++ src/about.rs
@@ -1,18 +1,43 @@
 use const_format::formatcp;
 use druid::{
-    widget::{CrossAxisAlignment, Flex, Label},
-    Widget, WidgetExt,
+    widget::{Button, Flex, Image, Label},
+    ImageBuf, Widget, WidgetExt,
 };
 
 use crate::AppState;
 
 const APP_VERSION: &str = formatcp!("Ice Launcher version {}", env!("CARGO_PKG_VERSION"));
-const LICENSE: &str = "GPL-3.0-only Licensed | © 2022 Manuel Quarneti";
+const REPOSITORY: &str = "https://github.com/mq1/ice-launcher";
+const LICENSE: &str = "https://github.com/mq1/ice-launcher/blob/main/COPYING";
+const COPYRIGHT: &str = "Copyright © 2022 Manuel Quarneti";
 
 pub fn build_widget() -> impl Widget<AppState> {
+    let png_data = ImageBuf::from_data(include_bytes!("../ice-launcher.png")).unwrap();
+    let image = Image::new(png_data).fix_width(128.);
+
     Flex::column()
-        .cross_axis_alignment(CrossAxisAlignment::Start)
-        .with_child(Label::new(APP_VERSION))
-        .with_child(Label::new(LICENSE))
+        .with_flex_spacer(1.)
+        .with_child(image)
+        .with_default_spacer()
+        .with_child(Label::new(APP_VERSION).with_text_size(32.))
+        .with_default_spacer()
+        .with_child(Label::new("Made with ❤️ by Manuel Quarneti").with_text_size(16.))
+        .with_flex_spacer(1.)
+        .with_child(
+            Flex::row()
+                .with_child(Button::new("Repository ↗️").on_click(
+                    |_ctx, _data: &mut AppState, _env: &_| {
+                        open::that(REPOSITORY).unwrap();
+                    },
+                ))
+                .with_default_spacer()
+                .with_child(Button::new("GPL-3.0-only Licensed ↗️").on_click(
+                    |_ctx, _data: &mut AppState, _env: &_| {
+                        open::that(LICENSE).unwrap();
+                    },
+                ))
+                .with_flex_spacer(1.)
+                .with_child(Label::new(COPYRIGHT)),
+        )
         .padding(10.)
 }
