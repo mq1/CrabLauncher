414a77c06a93a1eeffdd52108b73f60c88c9e133
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-24 16:02:38 +0200

    💄 Styling

--- src/settings.rs
+++ src/settings.rs
@@ -1,7 +1,15 @@
-use druid::{widget::Label, Widget, WidgetExt};
+use druid::{
+    widget::{CrossAxisAlignment, Flex, Label},
+    Widget, WidgetExt,
+};
 
 use crate::AppState;
 
 pub fn build_widget() -> impl Widget<AppState> {
-    Label::new("Settings").padding(10.)
+    Flex::column()
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .with_child(Label::new("⚙️ Settings").with_text_size(32.))
+        .with_default_spacer()
+        .with_flex_child(Label::new("TODO").with_text_size(24.), 1.)
+        .padding(10.)
 }
