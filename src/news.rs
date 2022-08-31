6a7906f481dd6669cf7290601e24482a9bf62c72
Manuel Quarneti <manuq01@pm.me>
2022-08-31 13:09:16 +0200

    💄 Loading spinner for news

--- src/news.rs
+++ src/news.rs
@@ -2,38 +2,50 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use druid::{
-    widget::{Button, CrossAxisAlignment, Flex, Label, List, Scroll},
-    Color, Widget, WidgetExt,
+    widget::{Button, CrossAxisAlignment, Either, Flex, Label, List, Scroll, Spinner},
+    Color, UnitPoint, Widget, WidgetExt,
 };
 
-use crate::{lib::{minecraft_news::MINECRAFT_NEWS_BASE_URL, self}, AppState};
+use crate::{
+    lib::{self, minecraft_news::MINECRAFT_NEWS_BASE_URL},
+    AppState,
+};
 
 pub fn build_widget() -> impl Widget<AppState> {
+    let loading = Flex::column()
+        .with_child(Label::new("Loading..."))
+        .with_default_spacer()
+        .with_child(Spinner::new())
+        .align_horizontal(UnitPoint::CENTER);
+
+    let news = Scroll::new(
+        List::new(|| {
+            Flex::row()
+                .with_child(Label::new(|(item, _): &(String, String), _env: &_| {
+                    item.to_string()
+                }))
+                .with_flex_spacer(1.)
+                .with_child(Button::new("Open ↗️").on_click(|_ctx, (_, url), _env: &_| {
+                    open::that(format!("{MINECRAFT_NEWS_BASE_URL}{url}")).unwrap();
+                }))
+                .padding(5.)
+                .border(Color::GRAY, 1.)
+                .rounded(5.)
+        })
+        .with_spacing(10.)
+        .lens(AppState::news),
+    )
+    .vertical();
+
+    let either = Either::new(|data: &AppState, _env| data.news.is_empty(), loading, news);
+
     Flex::column()
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .with_child(Label::new("🌎 News").with_text_size(32.))
         .with_default_spacer()
-        .with_flex_child(
-            Scroll::new(
-                List::new(|| {
-                    Flex::row()
-                        .with_child(Label::new(|(item, _): &(String, String), _env: &_| {
-                            item.to_string()
-                        }))
-                        .with_flex_spacer(1.)
-                        .with_child(Button::new("Open ↗️").on_click(|_ctx, (_, url), _env: &_| {
-                            open::that(format!("{MINECRAFT_NEWS_BASE_URL}{url}")).unwrap();
-                        }))
-                        .padding(5.)
-                        .border(Color::GRAY, 1.)
-                        .rounded(5.)
-                })
-                .with_spacing(10.)
-                .lens(AppState::news),
-            )
-            .vertical(),
-            1.,
-        )
+        .with_flex_spacer(1.)
+        .with_child(either)
+        .with_flex_spacer(1.)
         .padding(10.)
 }
 
