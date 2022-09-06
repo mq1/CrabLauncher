42d1fbb7a113a9b88939574003731e1ece2ab7db
Manuel Quarneti <manuq01@pm.me>
2022-09-06 15:10:43 +0200

    🚧 Instance creation

--- src/root.rs
+++ src/root.rs
@@ -9,7 +9,8 @@ use druid::{
 };
 
 use crate::{
-    about, accounts, install_runtime, instances, news, runtimes, settings, AppState, View, new_instance,
+    about, accounts, install_runtime, instance_name_selection, instance_type_selection,
+    instance_version_selection, instances, news, runtimes, settings, AppState, View,
 };
 
 pub fn build_widget() -> impl Widget<AppState> {
@@ -60,7 +61,9 @@ pub fn build_widget() -> impl Widget<AppState> {
         |data: &AppState, _env| data.current_view,
         |selector, _data, _env| match selector {
             View::Instances => Box::new(instances::build_widget()),
-            View::NewInstance => Box::new(new_instance::build_widget()),
+            View::InstanceTypeSelection => Box::new(instance_type_selection::build_widget()),
+            View::InstanceVersionSelection => Box::new(instance_version_selection::build_widget()),
+            View::InstanceNameSelection => Box::new(instance_name_selection::build_widget()),
             View::Accounts => Box::new(accounts::build_widget()),
             View::Runtimes => Box::new(runtimes::build_widget()),
             View::InstallRuntime => Box::new(install_runtime::build_widget()),
