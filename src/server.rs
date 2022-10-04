996850f78e733f8ff587f196f36d014ae757a67a
Manuel Quarneti <manuq01@pm.me>
2022-10-04 11:28:44 +0200

    ♻️ Refactoring

--- src/server.rs
+++ src/server.rs
@@ -14,18 +14,15 @@ pub async fn serve(event_sink: druid::ExtEventSink) {
         .map(move |p: HashMap<String, String>| match p.get("code") {
             Some(code) => {
                 let event_sink = event_sink.clone();
-                Runtime::new().unwrap().block_on(async move {
-                    lib::accounts::add(code.to_owned()).await.unwrap();
-                    let accounts = lib::accounts::read()
-                        .await
-                        .expect("Failed to list accounts")
-                        .accounts;
+                let rt = Runtime::new().unwrap();
+                rt.block_on(lib::accounts::add(code.to_owned())).unwrap();
+                let accounts = rt.block_on(lib::accounts::read()).unwrap().accounts;
 
-                    event_sink.add_idle_callback(move |data: &mut AppState| {
-                        data.accounts = accounts;
-                        data.current_view = View::Accounts;
-                    });
+                event_sink.add_idle_callback(move |data: &mut AppState| {
+                    data.accounts = accounts;
+                    data.current_view = View::Accounts;
                 });
+
                 "You can close this tab"
             }
             None => "No \"code\" param in query",
