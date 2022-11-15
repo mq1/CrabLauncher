4ad3fc6cb0d81a8471a6d015bc8d25094cd309d9
Manuel Quarneti <manuq01@pm.me>
2022-11-15 14:40:08 +0100

    📄 SPDX

--- src/style.rs
+++ src/style.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2022-present Manuel Quarneti <hi@mq1.eu>
+// SPDX-License-Identifier: GPL-3.0-only
+
 use iced::{color, theme, widget::container, Color, Theme};
 
 pub struct Card;
