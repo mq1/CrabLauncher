d1d69fc40a63bc43c03efdd02144b36af99244fd
Manuel Quarneti <manuelquarneti@gmail.com>
2022-11-18 20:37:51 +0100

    💄 Styling

--- src/style.rs
+++ src/style.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2022-present Manuel Quarneti <hi@mq1.eu>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use iced::{color, theme, widget::container, Color, Theme};
+use iced::{color, theme, widget::container, Background, Color, Theme};
 
 pub struct Card;
 
@@ -12,7 +12,8 @@ impl container::StyleSheet for Card {
         container::Appearance {
             border_radius: 5.0,
             border_width: 1.0,
-            border_color: color!(0x4b5563),
+            border_color: color!(0x3f3f46),
+            background: Some(Background::Color(color!(0x27272a))),
             ..Default::default()
         }
     }
