072cc0fefe2e9861615c4d43d1701739d406c587
Manuel Quarneti <manuq01@pm.me>
2022-11-28 16:49:08 +0100

    🐛 Maybe fix logo

--- src/about.rs
+++ src/about.rs
@@ -11,7 +11,7 @@ use crate::Message;
 const APP_NAME: &str = "Ice Launcher";
 const APP_VERSION: &str = concat!("v", env!("CARGO_PKG_VERSION"));
 const COPYRIGHT: &str = "GPL-3.0-only Licensed | Copyright © 2022 Manuel Quarneti";
-const LOGO_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/ice-launcher.png");
+const LOGO_BYTES: &[u8] = include_bytes!("../ice-launcher.png");
 const REPOSITORY: &str = "https://github.com/mq1/ice-launcher";
 const LICENSE: &str = "https://github.com/mq1/ice-launcher/blob/main/COPYING";
 
@@ -23,9 +23,12 @@ impl About {
     }
 
     pub fn view(&self) -> Element<Message> {
+        let logo_handle = image::Handle::from_memory(LOGO_BYTES.to_vec());
+        let logo = image::viewer(logo_handle).height(Length::Units(200));
+
         column![
             vertical_space(Length::Fill),
-            image(LOGO_PATH).height(Length::Units(200)),
+            logo,
             text(APP_NAME).size(50),
             text(APP_VERSION),
             text("Made with <3 in Rust by Manuel Quarneti"),
