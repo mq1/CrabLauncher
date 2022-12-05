dfce9cbe0db4b0e0b1555cd2edff297d6d94c7f7
Manuel Quarneti <manuelquarneti@gmail.com>
2022-12-05 17:28:12 +0100

    ♻️ Refactoring

--- client/src/vanilla_installer.rs
+++ client/src/vanilla_installer.rs
@@ -3,97 +3,50 @@
 
 use iced::{
     widget::{button, column, container, horizontal_space, pick_list, row, text, text_input},
-    Command, Element, Length,
+    Element, Length,
 };
 use mclib::minecraft_version_manifest::Version;
 
-use crate::style;
+use crate::{style, Message};
 
-#[derive(Debug, Clone)]
-pub enum Message {
-    FetchVersions,
-    FetchedVersions(Result<Vec<Version>, String>),
-    NewInstanceNameChanged(String),
-    VersionSelected(Version),
-    CreateInstance,
-}
+pub fn view<'a>(
+    name: &'a str,
+    versions: &'a Option<Result<Vec<Version>, String>>,
+    selected_version: &'a Option<Version>,
+) -> Element<'a, Message> {
+    let heading = text("New instance").size(50);
 
-pub struct VanillaInstaller {
-    pub name: String,
-    pub available_versions: Option<Result<Vec<mclib::minecraft_version_manifest::Version>, String>>,
-    pub selected_version: Option<mclib::minecraft_version_manifest::Version>,
-}
+    let instance_name = container(row![
+        text("Instance name"),
+        horizontal_space(Length::Fill),
+        text_input("Instance name", name, Message::NewInstanceNameChanged),
+    ])
+    .padding(10)
+    .style(style::card());
 
-impl VanillaInstaller {
-    pub fn new() -> Self {
-        Self {
-            name: String::new(),
-            available_versions: None,
-            selected_version: None,
-        }
-    }
+    let version: Element<_> = match versions {
+        Some(Ok(versions)) => pick_list(
+            versions,
+            selected_version.to_owned(),
+            Message::VanillaVersionSelected,
+        )
+        .into(),
+        Some(Err(error)) => text(error).into(),
+        None => text("Loading versions...").into(),
+    };
 
-    pub fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::FetchVersions => {
-                return Command::perform(
-                    async {
-                        mclib::minecraft_version_manifest::fetch_versions()
-                            .map_err(|e| e.to_string())
-                    },
-                    Message::FetchedVersions,
-                );
-            }
-            Message::FetchedVersions(versions) => {
-                self.available_versions = Some(versions);
-            }
-            Message::NewInstanceNameChanged(name) => {
-                self.name = name;
-            }
-            Message::VersionSelected(version) => {
-                self.selected_version = Some(version);
-            }
-            _ => {}
-        }
+    let version = container(row![
+        text("Minecraft version"),
+        horizontal_space(Length::Fill),
+        version,
+    ])
+    .padding(10)
+    .style(style::card());
 
-        Command::none()
-    }
+    let create_button = button("Create instance").on_press(Message::CreateVanillaInstance);
 
-    pub fn view(&self) -> Element<Message> {
-        let heading = text("New instance").size(50);
-
-        let instance_name = container(row![
-            text("Instance name"),
-            horizontal_space(Length::Fill),
-            text_input("Instance name", &self.name, Message::NewInstanceNameChanged),
-        ])
-        .padding(10)
-        .style(style::card());
-
-        let version: Element<_> = match &self.available_versions {
-            Some(Ok(versions)) => pick_list(
-                versions,
-                self.selected_version.to_owned(),
-                Message::VersionSelected,
-            )
-            .into(),
-            Some(Err(error)) => text(error).into(),
-            None => text("Loading versions...").into(),
-        };
-
-        let version = container(row![
-            text("Minecraft version"),
-            horizontal_space(Length::Fill),
-            version,
-        ])
-        .padding(10)
-        .style(style::card());
-
-        let create_button = button("Create instance").on_press(Message::CreateInstance);
-
-        column![heading, instance_name, version, create_button]
-            .padding(20)
-            .spacing(20)
-            .into()
-    }
+    column![heading, instance_name, version, create_button]
+        .padding(20)
+        .spacing(20)
+        .into()
 }
