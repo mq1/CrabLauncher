bcde901e410dcc934dcf3d97d66bd4e85bc10044
Manuel Quarneti <manuq01@pm.me>
2022-12-05 16:07:08 +0100

    ♻️ Refactoring

--- client/src/about.rs
+++ client/src/about.rs
@@ -15,35 +15,27 @@ const LOGO_BYTES: &[u8] = include_bytes!("../../ice-launcher.png");
 const REPOSITORY: &str = "https://github.com/mq1/ice-launcher";
 const LICENSE: &str = "https://github.com/mq1/ice-launcher/blob/main/COPYING";
 
-pub struct About;
+pub fn view() -> Element<'static, Message> {
+    let logo_handle = image::Handle::from_memory(LOGO_BYTES.to_vec());
+    let logo = image::viewer(logo_handle).height(Length::Units(200));
 
-impl About {
-    pub fn new() -> Self {
-        Self
-    }
-
-    pub fn view(&self) -> Element<Message> {
-        let logo_handle = image::Handle::from_memory(LOGO_BYTES.to_vec());
-        let logo = image::viewer(logo_handle).height(Length::Units(200));
-
-        column![
-            vertical_space(Length::Fill),
-            logo,
-            text(APP_NAME).size(50),
-            text(APP_VERSION),
-            text("Made with <3 in Rust by Manuel Quarneti"),
-            vertical_space(Length::Fill),
-            row![
-                button("Repository").on_press(Message::OpenURL(REPOSITORY.to_string())),
-                button("License").on_press(Message::OpenURL(LICENSE.to_string())),
-                horizontal_space(Length::Fill),
-                text(COPYRIGHT),
-            ]
-            .spacing(10),
+    column![
+        vertical_space(Length::Fill),
+        logo,
+        text(APP_NAME).size(50),
+        text(APP_VERSION),
+        text("Made with <3 in Rust by Manuel Quarneti"),
+        vertical_space(Length::Fill),
+        row![
+            button("Repository").on_press(Message::OpenURL(REPOSITORY.to_string())),
+            button("License").on_press(Message::OpenURL(LICENSE.to_string())),
+            horizontal_space(Length::Fill),
+            text(COPYRIGHT),
         ]
-        .spacing(20)
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
-    }
+        .spacing(10),
+    ]
+    .spacing(20)
+    .padding(20)
+    .align_items(Alignment::Center)
+    .into()
 }
