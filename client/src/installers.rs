7fb3d6359b9f734e0af84c851a6877e8e93e7375
Manuel Quarneti <manuq01@pm.me>
2022-12-05 16:12:41 +0100

    ♻️ Refactoring

--- client/src/installers.rs
+++ client/src/installers.rs
@@ -8,19 +8,11 @@ use iced::{
 
 use crate::Message;
 
-pub struct Installers;
+pub fn view() -> Element<'static, Message> {
+    let heading = text("Installers").size(50);
+    let vanilla_button = button("Vanilla").on_press(Message::OpenVanillaInstaller);
+    let modrinth_button = button("Modrinth").on_press(Message::OpenModrinthModpacks);
+    let installers = column![vanilla_button, modrinth_button].spacing(10);
 
-impl Installers {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    pub fn view(&self) -> Element<Message> {
-        let heading = text("Installers").size(50);
-        let vanilla_button = button("Vanilla").on_press(Message::OpenVanillaInstaller);
-        let modrinth_button = button("Modrinth").on_press(Message::OpenModrinthModpacks);
-        let installers = column![vanilla_button, modrinth_button].spacing(10);
-
-        column![heading, installers].spacing(20).padding(20).into()
-    }
+    column![heading, installers].spacing(20).padding(20).into()
 }
