05c8f618a1263f6c49be62481617f1121d6dfbd0
Manuel Quarneti <manuq01@pm.me>
2022-12-07 15:04:51 +0100

    ➖ Remove url dependency

--- mclib/src/lib.rs
+++ mclib/src/lib.rs
@@ -32,6 +32,14 @@ pub mod runtime_manager;
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
+#[macro_export]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
+
 pub static BASE_DIR: Lazy<PathBuf> = Lazy::new(|| {
     ProjectDirs::from("eu", "mq1", "ice-launcher")
         .expect("Could not get project directories")
