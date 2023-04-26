442fd04927cdde927db69428883de6a7662b3157
Manuel Quarneti <manuelquarneti@gmail.com>
2023-04-26 11:18:54 +0200

    ✨ Save settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,17 +1,17 @@
 // SPDX-FileCopyrightText: 2023 Manuel Quarneti <hi@mq1.eu>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{path::PathBuf, fs};
+use std::{fs, path::PathBuf};
 
 use anyhow::Result;
 use iced::{
-    widget::{button, column, horizontal_space, row, text, toggler},
+    widget::{button, column, container, horizontal_space, row, text, toggler, vertical_space},
     Element, Length,
 };
 use once_cell::sync::Lazy;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-use crate::{Message, View, BASE_DIR};
+use crate::{style, Message, View, BASE_DIR};
 
 pub static PATH: Lazy<PathBuf> = Lazy::new(|| BASE_DIR.join("settings.toml"));
 
@@ -58,9 +58,16 @@ impl Settings {
             button("Back").on_press(Message::ChangeView(View::Instances))
         ];
 
-        column![header, check_for_updates]
-            .spacing(10)
-            .padding(10)
-            .into()
+        let save_button = button("Save").on_press(Message::SaveSettings);
+
+        column![
+            header,
+            container(column![check_for_updates].padding(10)).style(style::card()),
+            vertical_space(Length::Fill),
+            row![horizontal_space(Length::Fill), save_button]
+        ]
+        .spacing(10)
+        .padding(10)
+        .into()
     }
 }
