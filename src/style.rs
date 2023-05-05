9c1004859d55da3648b0fea4387b2c2334c35466
Manuel Quarneti <manuq01@pm.me>
2023-05-05 14:35:43 +0200

    ♻️ Refactoring

--- src/style.rs
+++ src/style.rs
@@ -6,24 +6,32 @@ use iced::{
     widget::{button, container},
     Background, Theme,
 };
-pub struct Card;
+pub struct Card {
+    theme: theme::Container,
+}
+
+impl Card {
+    pub fn new(theme: theme::Container) -> Self {
+        Self { theme }
+    }
+}
 
 impl container::StyleSheet for Card {
     type Style = Theme;
 
-    fn appearance(&self, _style: &Self::Style) -> container::Appearance {
-        container::Appearance {
-            border_radius: 5.0,
-            border_width: 1.0,
-            border_color: color!(0x3f3f46),
-            background: Some(Background::Color(color!(0x27272a))),
-            ..Default::default()
-        }
+    fn appearance(&self, style: &Self::Style) -> container::Appearance {
+        let mut appearance = style.appearance(&self.theme);
+        appearance.border_radius = 5.0;
+        appearance.border_width = 1.0;
+        appearance.border_color = color!(0x3f3f46);
+        appearance.background = Some(Background::Color(color!(0x27272a)));
+
+        appearance
     }
 }
 
 pub fn card() -> theme::Container {
-    theme::Container::Custom(Box::new(Card))
+    theme::Container::Custom(Box::new(Card::new(theme::Container::default())))
 }
 
 pub struct CircleButtonStyle {
