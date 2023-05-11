ce392f9d8d02ee6d5284fdf0f4e9ca97dc36534d
Manuel Quarneti <manuq01@pm.me>
2023-05-11 10:34:01 +0200

    ✨ Updater

---
+++ src/util/updater.rs
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2023 Manuel Quarneti <hi@mq1.eu>
+// SPDX-License-Identifier: GPL-3.0-only
+
+use anyhow::Result;
+use serde::Deserialize;
+use version_compare::Version;
+
+use crate::util::USER_AGENT;
+
+const LATEST_RELEASE_URL: &str = "https://api.github.com/repos/mq1/icy-launcher/releases/latest";
+const RELEASES_BASE_URL: &str = "https://github.com/mq1/icy-launcher/releases/tag/";
+
+#[derive(Deserialize)]
+struct Release {
+    tag_name: String,
+}
+
+fn get_latest_release() -> Result<Release> {
+    let resp = ureq::get(LATEST_RELEASE_URL)
+        .set("User-Agent", USER_AGENT)
+        .call()?
+        .into_json()?;
+
+    Ok(resp)
+}
+
+pub async fn check_for_updates() -> Result<Option<(String, String)>> {
+    let latest_release = get_latest_release()?;
+    let latest_release = Version::from(&latest_release.tag_name).unwrap();
+    let current_version = Version::from(env!("CARGO_PKG_VERSION")).unwrap();
+
+    if latest_release > current_version {
+        let url = format!("{}{}", RELEASES_BASE_URL, latest_release);
+        return Ok(Some((latest_release.to_string(), url)));
+    }
+
+    Ok(None)
+}
