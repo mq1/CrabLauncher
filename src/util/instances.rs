f60707593fb4ac5b163d567a2a88525519125800
Manuel Quarneti <manuq01@pm.me>
2023-05-12 18:31:41 +0200

    ♻️ Refactor instances

--- src/util/instances.rs
+++ src/util/instances.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Manuel Quarneti <hi@mq1.eu>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{fs, path::PathBuf};
+use std::{fmt::Display, fs, path::PathBuf};
 
 use anyhow::Result;
 use once_cell::sync::Lazy;
@@ -13,15 +13,29 @@ use crate::BASE_DIR;
 pub static INSTANCES_DIR: Lazy<PathBuf> = Lazy::new(|| BASE_DIR.join("instances"));
 
 #[derive(Serialize, Deserialize)]
-pub struct Instance {
+pub struct InstanceInfo {
     last_played: Option<PrimitiveDateTime>,
 }
 
+pub struct Instance {
+    pub name: String,
+    pub info: InstanceInfo,
+}
+
 pub struct Instances {
-    pub list: Vec<String>,
+    pub list: Vec<Instance>,
 }
 
 impl Instances {
+    fn sort(&mut self) {
+        self.list.sort_by(|a, b| {
+            let a = a.info.last_played.unwrap_or_else(|| PrimitiveDateTime::MIN);
+            let b = b.info.last_played.unwrap_or_else(|| PrimitiveDateTime::MIN);
+
+            b.cmp(&a)
+        });
+    }
+
     pub fn load() -> Result<Self> {
         if !INSTANCES_DIR.exists() {
             fs::create_dir(&*INSTANCES_DIR)?;
@@ -39,30 +53,45 @@ impl Instances {
                 }
 
                 let path = entry.path();
-                let path = path.file_name()?.to_str()?.to_string();
-                Some(path)
+                let name: String = path.file_name()?.to_str()?.to_string();
+
+                let info_path = INSTANCES_DIR.join(&name).join("instance.toml");
+                let info = fs::read_to_string(info_path).ok()?;
+                let info = toml::from_str(&info).ok()?;
+
+                let instance = Instance { name, info };
+
+                Some(instance)
             })
             .collect();
 
-        Ok(Self { list })
+        let mut instances = Self { list };
+        instances.sort();
+
+        Ok(instances)
     }
 
-    pub fn new(name: &str) -> Result<()> {
+    pub fn new(&mut self, name: &str) -> Result<()> {
         let path = INSTANCES_DIR.join(name);
         fs::create_dir(&path)?;
 
-        let info = Instance { last_played: None };
-        let info = toml::to_string_pretty(&info)?;
-        fs::write(path.join("instance.toml"), info)?;
+        let info = InstanceInfo { last_played: None };
+        let info_str = toml::to_string_pretty(&info)?;
+        fs::write(path.join("instance.toml"), info_str)?;
+
+        self.list.push(Instance {
+            name: name.to_string(),
+            info,
+        });
+
+        self.sort();
 
         Ok(())
     }
+}
 
-    pub fn get_info(&self, name: &str) -> Result<Instance> {
-        let path = INSTANCES_DIR.join(name).join("instance.toml");
-        let info = fs::read_to_string(path)?;
-        let info: Instance = toml::from_str(&info)?;
-
-        Ok(info)
+impl Display for Instance {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
     }
 }
