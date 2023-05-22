758f3611f799715e3547bc4304a6c33b0d3ba3b3
Manuel Quarneti <manuel.quarneti@proton.me>
2023-05-22 15:59:41 +0200

    ♻️ Refactoring

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -10,56 +10,44 @@ use iced_aw::Spinner;
 use crate::{components::icons, style, Message, View};
 
 pub fn view<'a>(current_view: &'a View, account_head: &'a Option<Vec<u8>>) -> Element<'a, Message> {
-    let change_view_button = |view: &View| -> Element<Message> {
-        let icon = match view {
-            View::LatestInstance => icons::package(),
-            View::Instances => icons::grid(),
-            View::NewVanillaInstance => icons::minecraft(),
-            View::NewModrinthInstance => icons::modrinth(),
-            View::Accounts => {
-                if let Some(head) = account_head {
-                    if head.is_empty() {
-                        Spinner::new().into()
-                    } else {
-                        let head_handle = image::Handle::from_memory(head.clone());
-                        let head = Image::new(head_handle).width(32).height(32);
-
-                        head.into()
-                    }
-                } else {
-                    icons::account_alert().into()
-                }
-            }
-            View::Settings => icons::cog(),
-            View::About => icons::info(),
-            View::AddingAccount(_, _) => todo!(),
-            View::FullscreenMessage(_) => todo!(),
-        };
-
+    let change_view_button = |view: View, icon: Element<'static, Message>| -> Element<Message> {
         let mut btn = button(icon)
             .style(style::transparent_button())
             .padding(10)
             .on_press(Message::ChangeView(view.clone()));
 
-        if view == current_view {
+        if &view == current_view {
             btn = btn.style(style::selected_button());
         }
 
         btn.into()
     };
 
+    let account_icon = if let Some(head) = account_head {
+        if head.is_empty() {
+            Spinner::new().into()
+        } else {
+            let head_handle = image::Handle::from_memory(head.clone());
+            let head = Image::new(head_handle).width(32).height(32);
+
+            head.into()
+        }
+    } else {
+        icons::account_alert().into()
+    };
+
     let col = column![
-        change_view_button(&View::LatestInstance),
-        change_view_button(&View::Instances),
+        change_view_button(View::LatestInstance, icons::package()),
+        change_view_button(View::Instances, icons::grid()),
         vertical_space(Length::Fill),
         text("New"),
         vertical_space(5),
-        change_view_button(&View::NewVanillaInstance),
-        change_view_button(&View::NewModrinthInstance),
+        change_view_button(View::NewVanillaInstance, icons::minecraft()),
+        change_view_button(View::NewModrinthInstance, icons::modrinth()),
         vertical_space(Length::Fill),
-        change_view_button(&View::Accounts),
-        change_view_button(&View::Settings),
-        change_view_button(&View::About),
+        change_view_button(View::Accounts, account_icon),
+        change_view_button(View::Settings, icons::cog()),
+        change_view_button(View::About, icons::info()),
     ]
     .align_items(Alignment::Center);
 
