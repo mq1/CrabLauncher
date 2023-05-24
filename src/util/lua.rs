f2f59b2810d1db88cd1ad271e72aae00d91bad3f
Manuel Quarneti <manuq01@pm.me>
2023-05-24 21:15:16 +0200

    ✨ Implement lua methods

--- src/util/lua.rs
+++ src/util/lua.rs
@@ -1,10 +1,14 @@
 // SPDX-FileCopyrightText: 2023 Manuel Quarneti <hi@mq1.eu>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{fs, io::BufReader, path::PathBuf};
+use std::{
+    fs::{self, File},
+    io::{self, BufWriter},
+    path::PathBuf,
+};
 
 use anyhow::Result;
-use flate2::bufread::GzDecoder;
+use flate2::read::GzDecoder;
 use mlua::{ExternalResult, Function, Lua, LuaSerdeExt};
 use serde_json::Value;
 use tar::Archive;
@@ -16,6 +20,7 @@ const MODULES_URL: &str = "https://github.com/mq1/icy-launcher/archive/refs/head
 pub fn get_vm() -> Result<Lua> {
     let lua = Lua::new();
 
+    // fetch and parse json from uri
     let fetch_json = lua.create_function(|lua, uri: String| {
         let resp = ureq::get(&uri).call().to_lua_err()?;
         let json = resp.into_json::<Value>().to_lua_err()?;
@@ -23,6 +28,29 @@ pub fn get_vm() -> Result<Lua> {
         lua.to_value(&json)
     })?;
     lua.globals().set("fetch_json", fetch_json)?;
+
+    // download json from uri and write to file
+    let download_json = lua.create_function(|lua, (uri, path): (String, String)| {
+        let resp = ureq::get(&uri).call().to_lua_err()?;
+        let str = resp.into_string().to_lua_err()?;
+        let json = serde_json::from_str::<Value>(&str).to_lua_err()?;
+
+        // write json to file
+        fs::write(path, str).to_lua_err()?;
+
+        lua.to_value(&json)
+    })?;
+    lua.globals().set("download_json", download_json)?;
+
+    // download file from uri
+    let download_file = lua.create_function(|_, (uri, path): (String, String)| {
+        let resp = ureq::get(&uri).call().to_lua_err()?;
+        let mut writer = BufWriter::new(File::create(path).to_lua_err()?);
+        io::copy(&mut resp.into_reader(), &mut writer).to_lua_err()?;
+
+        Ok(())
+    })?;
+    lua.globals().set("download_file", download_file)?;
 
     Ok(lua)
 }
@@ -36,8 +64,7 @@ pub fn download_modules() -> Result<()> {
     }
 
     let resp = ureq::get(MODULES_URL).call()?;
-    let reader = BufReader::new(resp.into_reader());
-    let mut archive = Archive::new(GzDecoder::new(reader));
+    let mut archive = Archive::new(GzDecoder::new(resp.into_reader()));
 
     archive.unpack(BASE_DIR.as_path())?;
 
