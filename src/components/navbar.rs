3575a408df2dd0387ba1a1076a7c08f29c7a806b
Manuel Quarneti <manuelquarneti@gmail.com>
2023-07-06 14:36:41 +0200

    💄 Add tooltips

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use iced::{
-    widget::{button, column, container, image, vertical_space, Image},
+    theme,
+    widget::{button, column, container, image, tooltip, vertical_space, Image},
     Alignment, Element, Length,
 };
 use iced_aw::Spinner;
@@ -10,17 +11,24 @@ use iced_aw::Spinner;
 use crate::{components::icons, style, Message, View};
 
 pub fn view<'a>(current_view: &'a View, account_head: &'a Option<Vec<u8>>) -> Element<'a, Message> {
-    let change_view_button = |view: View, icon: Element<'static, Message>| -> Element<Message> {
-        button(icon)
-            .padding(10)
-            .style(if &view == current_view {
-                style::selected_button()
-            } else {
-                style::transparent_button()
-            })
-            .on_press(Message::ChangeView(view))
+    let change_view_button =
+        |view: View, icon: Element<'static, Message>, tooltip_text| -> Element<Message> {
+            tooltip(
+                button(icon)
+                    .padding(10)
+                    .style(if &view == current_view {
+                        style::selected_button()
+                    } else {
+                        style::transparent_button()
+                    })
+                    .on_press(Message::ChangeView(view)),
+                tooltip_text,
+                tooltip::Position::Right,
+            )
+            .gap(10)
+            .style(theme::Container::Box)
             .into()
-    };
+        };
 
     let account_icon = if let Some(head) = account_head {
         if head.is_empty() {
@@ -36,13 +44,13 @@ pub fn view<'a>(current_view: &'a View, account_head: &'a Option<Vec<u8>>) -> El
     };
 
     let col = column![
-        change_view_button(View::LatestInstance, icons::package()),
-        change_view_button(View::NewInstance, icons::package_plus()),
-        change_view_button(View::Instances, icons::grid()),
+        change_view_button(View::LatestInstance, icons::package(), "Latest Instance"),
+        change_view_button(View::NewInstance, icons::package_plus(), "New Instance"),
+        change_view_button(View::Instances, icons::grid(), "Instances"),
         vertical_space(Length::Fill),
-        change_view_button(View::Accounts, account_icon),
-        change_view_button(View::Settings, icons::cog()),
-        change_view_button(View::About, icons::info()),
+        change_view_button(View::Accounts, account_icon, "Accounts"),
+        change_view_button(View::Settings, icons::cog(), "Settings"),
+        change_view_button(View::About, icons::info(), "About Icy Launcher"),
     ]
     .align_items(Alignment::Center);
 
