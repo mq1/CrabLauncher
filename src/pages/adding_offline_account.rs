4ef892344fc02060204deb1a4013bfd52fefe012
Manuel Quarneti <manuq01@pm.me>
2023-07-23 16:47:33 +0200

    :lipstick: Styling

--- src/pages/adding_offline_account.rs
+++ src/pages/adding_offline_account.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use iced::{Alignment, Element, Length, theme};
-use iced::widget::{button, Column, text, text_input, vertical_space};
+use iced::widget::{button, Column, container, text, text_input, vertical_space};
 use crate::style;
 
 use crate::types::messages::Message;
@@ -11,9 +11,10 @@ pub fn view(username: &str) -> Element<Message> {
     let title = text("Adding offline account").size(30);
 
     let username_input = text_input("Username", username)
+        .width(200)
         .on_input(Message::OfflineAccountUsernameChanged);
 
-    let add_button = button("Add")
+    let add_button = button(container(text("Add")).padding(5))
         .on_press(Message::AddOfflineAccount)
         .style(style::circle_button(theme::Button::Primary));
 
