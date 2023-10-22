39887f25bb28546c70f723c3a5237678eff66d82
Manuel Quarneti <manuq01@pm.me>
2023-10-22 10:52:12 +0200

    :lipstick: Styling

--- src/components/settings.rs
+++ src/components/settings.rs
@@ -5,8 +5,12 @@ use eframe::egui;
 use egui_modal::{Modal, ModalStyle};
 
 pub fn settings_modal(ctx: &egui::Context) -> Modal {
+    let window_size = ctx.available_rect().size();
+
     let style = ModalStyle {
         body_alignment: egui::Align::Min,
+        default_height: Some(window_size.y * 0.75),
+        default_width: Some(window_size.x * 0.75),
         ..Default::default()
     };
 
