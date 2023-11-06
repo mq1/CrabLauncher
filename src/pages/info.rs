cea46d882e324895ee179708ad41d8a4d8f94bce
Manuel Quarneti <manuq01@pm.me>
2023-11-06 20:47:02 +0100

    :lipstick: Center info

--- src/pages/info.rs
+++ src/pages/info.rs
@@ -8,28 +8,31 @@ const LOGO: egui::ImageSource = egui::include_image!("../../assets/logo-128x128.
 
 pub fn show(ctx: &egui::Context, app: &mut App) {
     egui::CentralPanel::default().show(ctx, |ui| {
-        let logo = egui::Image::new(LOGO).fit_to_exact_size(egui::vec2(128., 128.));
-        ui.add(logo);
+        egui::Area::new("info")
+            .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::ZERO)
+            .show(ctx, |ui| {
+                ui.vertical_centered(|ui| {
+                    let logo = egui::Image::new(LOGO).fit_to_exact_size(egui::vec2(128., 128.));
+                    ui.add(logo);
 
-        ui.heading("CrabLauncher");
+                    ui.heading("CrabLauncher");
 
-        ui.separator();
+                    ui.add_space(16.);
 
-        ui.label(format!("Version: {}", env!("CARGO_PKG_VERSION")));
+                    ui.label(format!("Version: {}", env!("CARGO_PKG_VERSION")));
 
-        ui.add_space(8.);
+                    ui.add_space(8.);
 
-        ui.label(format!("License: {}", env!("CARGO_PKG_LICENSE")));
+                    ui.label(format!("License: {}", env!("CARGO_PKG_LICENSE")));
 
-        ui.add_space(8.);
+                    ui.add_space(8.);
 
-        ui.label(format!("Authors: {}", env!("CARGO_PKG_AUTHORS")));
+                    ui.label(format!("Authors: {}", env!("CARGO_PKG_AUTHORS")));
 
-        ui.add_space(8.);
+                    ui.add_space(8.);
 
-        ui.horizontal(|ui| {
-            ui.label("Repository:".to_string());
-            ui.hyperlink(env!("CARGO_PKG_REPOSITORY"));
-        });
+                    ui.hyperlink(env!("CARGO_PKG_REPOSITORY"));
+                });
+            });
     });
 }
