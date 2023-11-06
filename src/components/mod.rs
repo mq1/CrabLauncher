64400d7b0fc6002ddbebe11597c231414cb01859
Manuel Quarneti <manuq01@pm.me>
2023-11-06 17:45:04 +0100

    :fire: Remove about dialog

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Manuel Quarneti <manuelquarneti@protonmail.com>
 // SPDX-License-Identifier: GPL-2.0-only
 
-pub mod about;
 pub mod navbar;
 pub mod navigation_button;
