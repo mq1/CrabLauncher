60aedfd0126e313083b08eb69614ed4dcd9af4a7
Manuel Quarneti <manuq01@pm.me>
2023-11-07 08:57:39 +0100

    :construction: New instance page

--- src/pages/vanilla_installer.rs
+++ src/pages/vanilla_installer.rs
@@ -5,6 +5,10 @@ use crate::app::App;
 use eframe::egui;
 
 pub fn show(ctx: &egui::Context, app: &mut App) {
+    if app.vanilla_installer.versions.is_none() {
+        app.vanilla_installer.fetch_versions();
+    }
+
     egui::CentralPanel::default().show(ctx, |ui| {
         ui.heading("Vanilla Installer");
 
@@ -12,27 +16,31 @@ pub fn show(ctx: &egui::Context, app: &mut App) {
         ui.add_space(16.);
 
         ui.vertical(|ui| {
-            ui.heading("Name");
-            ui.text_edit_singleline(&mut app.vanilla_installer.name);
+            ui.group(|ui| {
+                ui.label("Name");
+                ui.text_edit_singleline(&mut app.vanilla_installer.name);
+            });
 
             ui.add_space(8.);
 
-            ui.heading("Version");
-            egui::ComboBox::from_id_source("version_selector")
-                .selected_text(&app.vanilla_installer.selected_version)
-                .show_ui(ui, move |ui| {
-                    if let Some(versions) = &app.vanilla_installer.versions {
-                        if let Some(versions) = versions.ready() {
-                            for version in versions {
-                                ui.selectable_value(
-                                    &mut app.vanilla_installer.selected_version,
-                                    version.clone(),
-                                    version,
-                                );
+            ui.group(|ui| {
+                ui.label("Version");
+                egui::ComboBox::from_id_source("version_selector")
+                    .selected_text(&app.vanilla_installer.selected_version)
+                    .show_ui(ui, move |ui| {
+                        if let Some(versions) = &app.vanilla_installer.versions {
+                            if let Some(versions) = versions.ready() {
+                                for version in versions {
+                                    ui.selectable_value(
+                                        &mut app.vanilla_installer.selected_version,
+                                        version.clone(),
+                                        version,
+                                    );
+                                }
                             }
                         }
-                    }
-                });
+                    });
+            });
         });
     });
 }
